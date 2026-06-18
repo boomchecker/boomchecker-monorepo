ee9c577fd95e5136f63e496995e2c52359516401
kamilherman99 <83425669+kamilherman99@users.noreply.github.com>
2026-06-18 16:32:41 +0200

    CubeMX + C imp.

---
+++ fw/bom-stmnode/drony/src/firmware/Inc/cmsis_compiler.h
@@ -0,0 +1,14 @@
+#ifndef __CMSIS_COMPILER_H
+#define __CMSIS_COMPILER_H
+
+#include <stdint.h>
+
+#ifndef __STATIC_INLINE
+#define __STATIC_INLINE static inline
+#endif
+
+#ifndef __STATIC_FORCEINLINE
+#define __STATIC_FORCEINLINE static inline
+#endif
+
+#endif
